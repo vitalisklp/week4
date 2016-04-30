be9d7cf7a3d86f875cbc9d009cb7f029e0443dc4
Vitalijus Lykovas <L.Vitalijus@gmail.com>
2016-04-30 16:45:25 +0200

    req13

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,23 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_last_name_not_null, :male_name_not_Sue
+  validates :gender, inclusion: { in: %w(male female)} 
+
+  def first_last_name_not_null
+  	if first_name == nil and last_name == nil
+  		errors.add(:first_name, "You should enter at least first name or last name!")
+  	end
+  end
+
+  def male_name_not_Sue
+  	if gender == "male" and first_name == "Sue"
+  		errors.add(:first_name, "Sue is not allowed!")
+  	end
+  end
+
+  def get_all_profiles
+
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.destroy_all
 TodoList.destroy_all
 
