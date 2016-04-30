c0c5449ba36f57a660f223c023007fc5c38fa2e6
Vitalijus Lykovas <L.Vitalijus@gmail.com>
2016-04-30 15:24:01 +0200

    seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,47 +15,18 @@ persons = User.create! ([
 	{ username: "Clinton", password_digest: "Clinton123" }
 	])
 
-
 persons[0].create_profile first_name: "Carly", last_name: "Fiorina", gender: "female", birth_year: 1954
 persons[1].create_profile first_name: "Donald", last_name: "Trump", gender: "male", birth_year: 1946
 persons[2].create_profile first_name: "Ben", last_name: "Carson", gender: "male", birth_year: 1951
 persons[3].create_profile first_name: "Hillary", last_name: "Clinton", gender: "female", birth_year: 1947
 
-persons[0].todo_lists.create! [{list_name: "Fiorina's list", list_due_date: Date.today + 1.year}]
-persons[1].todo_lists.create! [{list_name: "Trump's list", list_due_date: Date.today + 1.year}]
-persons[2].todo_lists.create! [{list_name: "Carson's list", list_due_date: Date.today + 1.year}]
-persons[3].todo_lists.create! [{list_name: "Clinton's list", list_due_date: Date.today + 1.year}]
 
+persons.each do |p|
+	p.todo_lists.create! [ {list_name: "#{p.username}'s", list_due_date: Date.today + 1.year} ]
+end
 
-
-
-
-#TodoItem.create due_date: Date.today + 1.year, title: "Fiorina's item 1", description: "item 1", completed: true, TodoList_id: todolist1.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Fiorina's item 2", description: "item 1", completed: true, TodoList_id: todolist1.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Fiorina's item 3", description: "item 1", completed: true, TodoList_id: todolist1.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Fiorina's item 4", description: "item 1", completed: true, TodoList_id: todolist1.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Fiorina's item 5", description: "item 1", completed: true, TodoList_id: todolist1.id
-
-#todolist2 = TodoList.create list_name: "Trump's list", list_due_date: Date.today + 1.year, User_id: person2.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Trump's item 1", description: "item 1", completed: true, TodoList_id: todolist2.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Trump's item 2", description: "item 1", completed: true, TodoList_id: todolist2.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Trump's item 3", description: "item 1", completed: true, TodoList_id: todolist2.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Trump's item 4", description: "item 1", completed: true, TodoList_id: todolist2.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Trump's item 5", description: "item 1", completed: true, TodoList_id: todolist2.id
-
-#todolist3 = TodoList.create list_name: "Carson's list", list_due_date: Date.today + 1.year, User_id: person3.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Carson's item 1", description: "item 1", completed: true, TodoList_id: todolist3.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Carson's item 2", description: "item 1", completed: true, TodoList_id: todolist3.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Carson's item 3", description: "item 1", completed: true, TodoList_id: todolist3.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Carson's item 4", description: "item 1", completed: true, TodoList_id: todolist3.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Carson's item 5", description: "item 1", completed: true, TodoList_id: todolist3.id
-
-#todolist4 = TodoList.create list_name: "Clinton's list", list_due_date: Date.today + 1.year, User_id: person4.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Clinton's item 1", description: "item 1", completed: true, TodoList_id: todolist4.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Clinton's item 2", description: "item 1", completed: true, TodoList_id: todolist4.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Clinton's item 3", description: "item 1", completed: true, TodoList_id: todolist4.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Clinton's item 4", description: "item 1", completed: true, TodoList_id: todolist4.id
-#TodoItem.create due_date: Date.today + 1.year, title: "Clinton's item 5", description: "item 1", completed: true, TodoList_id: todolist4.id
-
-
-
+persons.each do |p|
+	(1..5).each do |i|
+		p.todo_lists[0].todo_items.create! [ {due_date: Date.today + 1.year, title: "#{p.username}'s item", description: "item #{i}", completed: true} ] 
+	end	
+end
