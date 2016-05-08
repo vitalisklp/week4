05b9637eb748d9d6bada4ff0025a6a99e4d75b8b
Vitalijus Lykovas <L.Vitalijus@gmail.com>
2016-05-08 22:00:09 +0200

    security rq10c

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(username: params[:user][:username])
+  	password = params[:user][:password]
+
+  	if user && user.authenticate(password)
+  		session[:user_id] = user.id
+  		redirect_to root_path, notice: "Logged in successfully"
+  	else
+  		redirect_to login_path, alert: "Invalid username/password combination"
+	end  
   end
 
   def destroy
